ac49c51b2bc62031f62d294e6e337c38ceb05743
Darth Vanger <darthvanger@gmail.com>
2016-07-11 22:32:08 +0300

    Added 'view-thoughts' page, which views files from google drive.

--- src/app-start.js
+++ src/app-start.js
@@ -1,15 +1,26 @@
 require.config({
+    baseUrl: 'src/',
     paths: {
         text: "../bower_components/text/text"
     }
 });
 
-define(['text!thought/create-thought/create-thought.html'], function(createThoughtTemplate) {
+define([
+    'text!thought/create-thought/create-thought.html',
+    'text!thought/view-thoughts/view-thoughts.html'
+],
+function(
+    createThoughtTemplate,
+    viewThoughtsTemplate
+) {
 
     $(document).ready(function() {
         console.debug('app start');
-        
-        $('.site-content').append(createThoughtTemplate);
+
+        //$('.create-thought').append(createThoughtTemplate);
+        $('.view-thoughts').append(viewThoughtsTemplate);
+
+        require(['src/thought/view-thoughts/view-thoughts.js']);
 
     });
 });
---
+++ src/thought/view-thoughts/view-thoughts.js
@@ -0,0 +1,96 @@
+console.debug('view-htoughts.js');
+require(['https://apis.google.com/js/client.js?onload=checkAuth"'], function() {
+      // Your Client ID can be retrieved from your project in the Google
+console.debug('gapi: ', gapi);
+      // Developer Console, https://console.developers.google.com
+      var CLIENT_ID = '586695064067-2k8v88rq1litcqj8v0ofnstj6t6qfhpa.apps.googleusercontent.com';
+
+      var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+
+        $('#authorize-button').on('click', handleAuthClick);
+
+      /**
+       * Check if current user has authorized this application.
+       */
+      function checkAuth() {
+        gapi.auth.authorize(
+          {
+            'client_id': CLIENT_ID,
+            'scope': SCOPES.join(' '),
+            'immediate': true
+          }, handleAuthResult);
+      }
+
+      /**
+       * Handle response from authorization server.
+       *
+       * @param {Object} authResult Authorization result.
+       */
+      function handleAuthResult(authResult) {
+        var authorizeDiv = document.getElementById('authorize-div');
+        console.debug('authResult: ', authResult);
+        if (authResult && !authResult.error) {
+          // Hide auth UI, then load client library.
+          authorizeDiv.style.display = 'none';
+          loadDriveApi();
+        } else {
+          // Show auth UI, allowing the user to initiate authorization by
+          // clicking authorize button.
+          authorizeDiv.style.display = 'inline';
+        }
+      }
+
+      /**
+       * Initiate auth flow in response to user clicking authorize button.
+       *
+       * @param {Event} event Button click event.
+       */
+      function handleAuthClick(event) {
+        gapi.auth.authorize(
+          {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+          handleAuthResult);
+        return false;
+      }
+
+      /**
+       * Load Drive API client library.
+       */
+      function loadDriveApi() {
+        gapi.client.load('drive', 'v3', listFiles);
+      }
+
+      /**
+       * Print files.
+       */
+      function listFiles() {
+        var request = gapi.client.drive.files.list({
+            'pageSize': 10,
+            'fields': "nextPageToken, files(id, name)"
+          });
+
+          request.execute(function(resp) {
+            appendPre('Files:');
+            var files = resp.files;
+            if (files && files.length > 0) {
+              for (var i = 0; i < files.length; i++) {
+                var file = files[i];
+                appendPre(file.name + ' (' + file.id + ')');
+              }
+            } else {
+              appendPre('No files found.');
+            }
+          });
+      }
+
+      /**
+       * Append a pre element to the body containing the given message
+       * as its text node.
+       *
+       * @param {string} message Text to be placed in pre element.
+       */
+      function appendPre(message) {
+        var pre = document.getElementById('output');
+        var textContent = document.createTextNode(message + '\n');
+        pre.appendChild(textContent);
+      }
+});

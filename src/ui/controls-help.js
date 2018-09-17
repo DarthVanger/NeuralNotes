2067871fb7b0d6f082c56119db46e72294248cb8
DarthVanger <darthvanger@gmail.com>
2018-09-17 23:19:34 +0300

    Add close icon to controls help

--- src/ui/controls-help.js
+++ src/ui/controls-help.js
@@ -13,17 +13,24 @@ define([
         element.style.position = 'absolute';
         element.style.bottom = 0;
         element.style.left = 0;
-        element.style.height = '6em';
-        element.style.padding = '1em';
+        element.style.height = '5em';
+        element.style.padding = '.5em';
         element.style.backgroundColor = 'yellow';
+
+        var closeIcon = document.createElement('SPAN');
+        closeIcon.innerText = 'X';
+        closeIcon.style.float = 'right';
+
 
         element.addEventListener('click', unmount);
 
         element.innerHTML = 'Controls:' + '<br />'
             + 'Double-click: add a child note' + '<br />'
-            + 'Hold: edit note\'s name' + '<br />';
+            + 'Hold: edit note\'s name';
 
         document.body.appendChild(element);
+
+        element.prepend(closeIcon);
 
     }
 

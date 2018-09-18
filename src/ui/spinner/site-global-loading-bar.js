0ee49217b55449a10fa4670c4d50167d0d2c4fb2
DarthVanger <darthvanger@gmail.com>
2018-09-19 00:58:38 +0300

    Delete some old commented code

--- src/ui/spinner/site-global-loading-bar.js
+++ src/ui/spinner/site-global-loading-bar.js
@@ -5,19 +5,15 @@ define([ ], function() {
     var $loadersContainer =  $('#site-global-loading-bar-messages-container');
     var $loadingBar = $('#site-global-loading-bar');
 
-    // loading bar is hidden by default, obviously :)
     $loadingBar.hide();
 
     var $loadingMessageElement = $('<div class="site-global-loading-bar__message"></div>');
-    //console.debug('site-global-loading-bar: $loadingMessageElement: ', $loadingMessageElement);
 
     var loaderInstances = [];
 
     var loadingBarService = {
-        // easy show/hide, without creating named instance
         show: showLoadingBar,
         hide: hideLoadingBar,
-        // create named instance of a loader
         create: create,
     };
 
@@ -27,7 +23,6 @@ define([ ], function() {
     return loadingBarService;
 
     function create(name) {
-        //console.debug('site-global-loading-bar: creating loader for name: ', name);
         var $msgEl = $loadingMessageElement.clone();
         $msgEl.append($('<div>Loading: ' + name + '</div>'));
         $msgEl.addClass('hidden-message');
@@ -38,10 +33,8 @@ define([ ], function() {
                 return loadingBarService.create(name + ' | ' + subName);
             },
             show: function() {
-                //console.debug('site-global-loading-bar: showing message element for message: ', name);
 
                 if (!isShown) {
-                    //console.debug('site-global-loading-bar: appending message element for message: ', name);
                     $loadersContainer.append($msgEl);
                 }
                 $msgEl.removeClass('hidden-message');
@@ -50,30 +43,24 @@ define([ ], function() {
                 isShown = true;
             },
             hide: function() {
-                //console.debug('site-global-loading-bar: removing message element for message:', name);
                 $msgEl.addClass('hidden-message');
                 hideLoadingBar(name);
-                // remove element after css animation.
                 var REMOVE_ANIMATION_DURATION = 2000;
                 setTimeout(function() {
                     $msgEl.remove();
                 }, REMOVE_ANIMATION_DURATION);
-                //loaderInstances.splice(loaderInstances.indexOf(loaderInstance, 1));
             }
         }
         loaderInstances.push(loaderInstance);
-        //console.debug('loadingBar: loader intances: ', loaderInstances);
         return loaderInstance;
     }
 
     function showLoadingBar() {
-        //console.debug('site-global-loading-bar: showLoadingBar()');
         $loadingBar.show();
         $backgroundSpinner.addClass('show');
     }
 
     function hideLoadingBar(name) {
-        //console.debug('site-global-loading-bar: hideLoadingBar()');
         $loadingBar.hide();
         $backgroundSpinner.removeClass('show');
     }

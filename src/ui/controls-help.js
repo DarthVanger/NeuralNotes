5f22d4c000cd7d301bf0f0d2a79f7f903390527b
Nick Nikoliuk <mykola.nikoliuk@gmail.com>
2019-04-06 19:47:22 +0300

    Refactor components using React

--- src/ui/controls-help.js
+++ src/ui/controls-help.js
@@ -1,48 +1,31 @@
-let element;
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
-export default {render};
+export class ControlsHelp extends Component {
 
-function render() {
-  if (wasSeenByUser()) {
-    console.info('Not rendering controls help as user has seen it already');
-    return;
+  render() {
+    const {onClick} = this.props;
+
+    const styles = {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      height: '5em',
+      padding: '.5em',
+      backgroundColor: 'yellow',
+    };
+
+    return (
+      <div id="tutorial" style={styles} onClick={onClick}>
+        <span style={{float: 'right', cursor: 'pointer'}}>X</span>
+        Controls:<br/>
+        Double-click: add a child note<br/>
+        Hold: edit note's name
+      </div>
+    );
   }
-
-  element = document.createElement('DIV');
-  element.id = 'tutorial';
-  element.style.position = 'absolute';
-  element.style.bottom = 0;
-  element.style.left = 0;
-  element.style.height = '5em';
-  element.style.padding = '.5em';
-  element.style.backgroundColor = 'yellow';
-
-  let closeIcon = document.createElement('SPAN');
-  closeIcon.innerText = 'X';
-  closeIcon.style.float = 'right';
-
-
-  element.addEventListener('click', unmount);
-
-  element.innerHTML = 'Controls:' + '<br />'
-    + 'Double-click: add a child note' + '<br />'
-    + 'Hold: edit note\'s name';
-
-  document.body.appendChild(element);
-
-  element.prepend(closeIcon);
-
 }
 
-function unmount() {
-  recordUserView();
-  element.remove();
-}
-
-function recordUserView() {
-  localStorage.setItem('controls_help:viewed', '1');
-}
-
-function wasSeenByUser() {
-  return localStorage.getItem('controls_help:viewed');
-}
+ControlsHelp.propTypes = {
+  onClick: PropTypes.func
+};

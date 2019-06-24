6e8a6c2a7f2f8b886a92c80bb19d477df49a46f5
Nick Nikoliuk <mykola.nikoliuk@gmail.com>
2019-06-24 23:34:23 +0300

    Refactor NoteNameEditor component and helpers

---
+++ src/components/NoteNameEditor/HeaderComponent/HeaderStyles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const StyledHeader = styled.div`
+
+  position: absolute;
+  top: 0;
+  z-index: 3;
+  height: 1.5em;
+  width: 100%;
+  background-color: #aac;
+  padding: 0.25em;
+  color: black;
+  text-transform: uppercase;
+`;
+
+export const StyledButtonContainer = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  text-align: right;
+`;
---
+++ src/components/NoteNameEditor/NoteNameEditorComponent.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import _ from "underscore";
+
+import { HeaderComponent } from 'components/NoteNameEditor/HeaderComponent/HeaderComponent';
+import {
+  StyledNoteEditorTextArea,
+  StyledNoteNameEditor
+} from "components/NoteNameEditor/NoteNameEditorStyles";
+
+export class NoteNameEditorComponent extends Component {
+  state = { name: "", propName: "" };
+
+  ref = React.createRef();
+
+  debouncedOnChange = _.debounce(name => this.props.onChange(name), 1500);
+
+  render() {
+    const { onDeleteClick, onUploadFileClick } = this.props;
+
+    return (
+      <StyledNoteNameEditor>
+        <HeaderComponent
+          onDeleteClick={onDeleteClick}
+          onUploadFileClick={onUploadFileClick}
+        />
+        <StyledNoteEditorTextArea
+          ref={this.ref}
+          onChange={this.onChange}
+          value={this.state.name}
+        />
+      </StyledNoteNameEditor>
+    );
+  }
+
+  static getDerivedStateFromProps({ note }, state) {
+    const { name } = note;
+    if (name !== state.propName) {
+      return { name, propName: name };
+    }
+    return null;
+  }
+
+  onChange = () => {
+    const name = this.ref.current.value;
+    this.debouncedOnChange(name);
+    this.setState({ name });
+  };
+}
+
+NoteNameEditorComponent.propTypes = {
+  note: PropTypes.object.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onDeleteClick: PropTypes.func.isRequired,
+  onUploadFileClick: PropTypes.func.isRequired
+};

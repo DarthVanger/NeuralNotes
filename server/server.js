2c57e52489fe1d6ad4122e7b5b834cf8de74bb09
Nick Nikoliuk <mykola.nikoliuk@gmail.com>
2019-07-13 19:06:21 +0300

    Disable built-in hot reload for webpack server; Disable host checking;

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import WebpackDevServer from 'webpack-dev-server';
 import log from './logger';
 
 const options = {
-  hot: true,
+  hot: false,
   publicPath: path.resolve(__dirname, '/dist'),
   port: 3000,
   host: 'localhost',
@@ -15,5 +15,5 @@ log.info('Starting dev server');
 
 WebpackDevServer.addDevServerEntrypoints(webpackConfig, options);
 
-const server = new WebpackDevServer(webpack(webpackConfig));
+const server = new WebpackDevServer(webpack(webpackConfig), { disableHostCheck: true });
 server.listen(options.port, () => log.info('listening on *:3000'));

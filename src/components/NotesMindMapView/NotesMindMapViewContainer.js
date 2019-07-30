06efd6cdc2f2ef01556a71f3bea129c4dc1e5f29
Nick Nikoliuk <mykola.nikoliuk@gmail.com>
2019-07-30 21:12:50 +0300

    refactor NotesMindMapView component; removed google api multiple initialization; removed unnecessary comments;

--- src/components/NotesMindMapView/NotesMindMapViewContainer.js
+++ src/components/NotesMindMapView/NotesMindMapViewContainer.js
@@ -2,6 +2,7 @@ import { connect } from 'react-redux';
 import { action } from 'sagas';
 
 import {
+  CHANGE_NOTE_VIS_NETWORK_NOTE_ACTION,
   CHANGE_SELECTED_NOTE_ACTION,
   REQUEST_NOTE_TEXT_ACTION
 } from 'components/NotesMindMapView/NotesMindMapViewActions';
@@ -16,6 +17,7 @@ const mapStateToProps = ({ notesMindMap: { selectedNote } }) => {
 const mapDispatchToProps = () => ({
   requestNoteText: data => action(REQUEST_NOTE_TEXT_ACTION, data),
   changeSelectedNote: data => action(CHANGE_SELECTED_NOTE_ACTION, data),
+  changeVisNetworkNote: data => action(CHANGE_NOTE_VIS_NETWORK_NOTE_ACTION, data),
 });
 
 export const NotesMindMapViewContainer = connect(

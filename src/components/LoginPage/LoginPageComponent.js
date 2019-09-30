4479335022546fd63f1804f5159f8cb921638d26
tomaTomoe <denysiuk.tina@gmail.com>
2019-09-30 18:13:20 +0300

    Implement new design for login page

--- src/components/LoginPage/LoginPageComponent.js
+++ src/components/LoginPage/LoginPageComponent.js
@@ -1,67 +1,101 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import slide1 from 'components/LoginPage/images/slide-1.png';
-import slide2 from 'components/LoginPage/images/slide-2.png';
-import slide3 from 'components/LoginPage/images/slide-3.png';
-import slide4 from 'components/LoginPage/images/slide-4.png';
+import styled from 'styled-components';
+import Slide1 from 'components/LoginPage/Slide1';
+import Slide2 from 'components/LoginPage/Slide2';
+import Slide3 from 'components/LoginPage/Slide3';
+import Slide4 from 'components/LoginPage/Slide4';
+import Slide5 from 'components/LoginPage/Slide5';
+import iconGoogle from 'components/LoginPage/images/icon-google.svg';
+import COLORS from 'components/LoginPage/colors';
+import { ASPECT_RATIO } from 'components/LoginPage/slideComponents';
+import { FONT_SIZE } from 'components/LoginPage/slideComponents';
+
+const Main = styled.main`
+  width: 100%;
+  background-color: ${COLORS.rainfull};
+  font-family: Roboto;
+`;
+
+const Article = styled.article`
+  width: 100%;
+  max-width: calc(100vh * ${ASPECT_RATIO});
+  margin: auto;
+`;
+
+const H3 = styled.h3`
+  padding-bottom: 7%;
+  font-size: ${FONT_SIZE.H3};
+  text-align: center;
+  color: ${COLORS.white};
+`;
+
+// 2% - indent for image positioning
+// 92% - image height without vertical indention
+// 4% - vertical indention
+// 0.75% - horizontal indention
+const GoogleLoginButton = styled.button`
+  color: ${COLORS.white};
+  background-color: ${COLORS.blue};
+  margin: auto;
+  position: relative;
+  border: none;
+  border-radius: 0.4vmin;
+  padding: 2vmin 4vmin 2vmin calc(9vmin + 2%); 
+
+  img {
+    height: 92%;
+    background-color: ${COLORS.white};
+    position: absolute;
+    padding: 3%;
+    left: 0.75%;
+    top: 4%;
+    border-radius: 0.4vmin;
+  }
+  
+  span {
+    vertical-align: middle;
+    line-height: 100%;
+    font-size: 3vmin;
+  }
+`;
+
+const Footer = styled.footer`
+  width: 100%;
+  max-width: calc(100vh * ${ASPECT_RATIO});
+  margin: auto;
+  text-align: center;
+  padding-bottom: 10%;
+
+  H3 {
+    padding-bottom: 3%;
+
+    span {
+      color: ${COLORS.aqua};
+    }
+  }
+`;
 
 export class LoginPageComponent extends Component {
   render() {
     const authHandler = this.props.isGoogleApiInitialized ? this.props.requestAuthorization : null;
     return (
-      <div>
-        <div className="container-fluid">
-          <h3 style={{ display: "inline-block" }}>NeuralNotes &mdash; organize your notes</h3>
-          <a onClick={authHandler} className="btn btn-social btn-lg btn-google"
-             style={{ width: '250px', display: 'inline-block', marginLeft: '20px', float: 'right', marginTop: '15px' }}>
-            <span className="fa fa-google"/>
-            Sign in with Google
-          </a>
-
-          <div style={{ height: '15px' }}/>
-
-          <div style={{ textAlign: 'center' }}>
-            <div className="slide">
-              <h2>Mind map your notes & files</h2>
-              <div className="slide-text">
-                <p>Your folders &amp; files as a mind map</p>
-                <p>Your mind map is stored as folders &amp; file</p>
-              </div>
-              <img src={slide1} style={{ width: '100%', maxWidth: '750px' }}/>
-            </div>
-            <div className="slide">
-              <h2>Explore subfolders by moving screen around</h2>
-              <div className="slide-text">
-                <p>You can see the entire structure, not just one level</p>
-              </div>
-              <img src={slide2} style={{ width: '100%', maxWidth: '750px' }}/>
-            </div>
-            <div className="slide">
-              <h2>Frequently used folders are BIGGER</h2>
-              <div className="slide-text">
-                <p>Every time you use a folder it becomes bigger</p>
-                <p>Old files you never use get smaller, drowning into deep memory</p>
-              </div>
-              <img src={slide3} style={{ width: '100%', maxWidth: '750px' }}/>
-            </div>
-            <div className="slide">
-              <h2>Single place for notes and files</h2>
-              <div className="slide-text">
-                <p>In NeuralNotes, every folder has a text file for the notes</p>
-                <p>It is displayed in a built-in text editor when a folder is selected</p>
-              </div>
-              <img src={slide4} style={{ width: '100%', maxWidth: '750px' }}/>
-            </div>
-          </div>
-
-          <h2 style={{ textAlign: 'center' }}>Sign up to NeuralNotes for free</h2>
-          <a onClick={authHandler} className="btn btn-social btn-lg btn-google"
-             style={{ width: '250px', display: 'block', margin: '1em auto' }}>
-            <span className="fa fa-google"/>
-            Sign up with Google
-          </a>
-        </div>
-      </div>
+      <Main>
+        <Article>
+          <Slide1 />
+          <Slide2 />
+          <Slide3 />
+          <Slide4 />
+          <Slide5 />
+          <Footer>
+            <H3><span>Neural</span>Notes is open source and free</H3>
+            <GoogleLoginButton type="button" onClick={authHandler}>
+              <img src={iconGoogle} />
+              <span>Sing up with Google</span>
+            </GoogleLoginButton>
+          </Footer>
+        </Article>
+      </Main>
     );
   }
 }

2b2e5788ee5d0d66d245325550777130c320cb19
tomaTomoe <denysiuk.tina@gmail.com>
2019-09-30 18:13:20 +0300

    Implement new design for login page

---
+++ src/components/LoginPage/slideComponents.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Img = styled.img`
+  width: 100%;
+  height: auto;
+  filter: ${props => props.shadow ? "drop-shadow( 0px 7px 4px rgba(0, 0, 0, .3))" : "none"} ;
+`;
+
+export const Caption = styled.svg`
+  font-weight: ${props => props.fontWeight || 300};
+  position: absolute;
+  width: ${props => props.width};
+  top: ${props => props.top};
+  left: ${props => props.left};
+  right: ${props => props.right};
+  bottom: ${props => props.bottom};
+`;
+
+//aspect ratio of the slide in the layout (375px/334px)
+export const ASPECT_RATIO = 375/334;   
+
+export const FONT_SIZE = {
+  H1: '5vmin',
+  H2: '3.5vmin',
+  H3: '2.5vmin',
+};

c271641f2acf0ded121fbb2bb194227b9586e922
tomaTomoe <denysiuk.tina@gmail.com>
2019-10-21 23:31:42 +0300

    bugfix - CaptionsConteiner had size different from image, Captions positions fixed

--- src/components/LoginPage/LoginPageComponents/Slide2.js
+++ src/components/LoginPage/LoginPageComponents/Slide2.js
@@ -28,8 +28,8 @@ const CaptionsConteiner = styled.div`
   position: absolute;
   top: 0;
   bottom: 0;
-  left: 0;
-  right: 0;
+  left: 13%;
+  right: 13%;
   padding: 0;
   color: #ffffff;
   text-align: center;
@@ -54,70 +54,70 @@ export default class Slide2 extends Component {
             <Figure>
               <Img src={slideImage} />
               <CaptionsConteiner>
-                <Caption left="23.5%" right="69%" bottom="84%" font="300">
+                <Caption left="14.5%" right="76%" bottom="84.5%" font="300">
                   <svg viewBox="0 0 60 15">
                     <text x="0" y="12" fill="#96FFFF">Chill</text>
                   </svg>
                 </Caption>
-                <Caption left="16.5%" right="76%" bottom="61%" font="300">
+                <Caption left="4.5%" right="86%" bottom="60%" font="300">
                   <svg viewBox="0 0 60 15">
                     <text x="0" y="12" fill="#96FFFF">Rock</text>
                   </svg>
                 </Caption>
-                <Caption left="16.5%" right="76%" bottom="24.5%" font="300">
+                <Caption left="4.5%" right="85%" bottom="24.5%" font="300">
                   <svg viewBox="0 0 60 15">
                     <text x="0" y="12" fill="#96FFFF">Art</text>
                   </svg>
                 </Caption>
-                <Caption left="47%" right="45.5%" bottom="8.5%" font="300">
+                <Caption left="46.5%" right="44%" bottom="7.5%" font="300">
                   <svg viewBox="0 0 60 15">
                     <text x="0" y="12" fill="#96FFFF">Quotes</text>
                   </svg>
                 </Caption>
-                <Caption left="50%" right="43.5%" bottom="85%" font="300">
+                <Caption left="50%" right="41%" bottom="85%" font="300">
                   <svg viewBox="0 0 60 36">
                     <text x="0" y="12" fill="#96FFFF">Berlin</text>
                     <text x="2" y="32" fill="#96FFFF">2019</text>
                   </svg>
                 </Caption>
-                <Caption left="78.5%" right="15.5%" bottom="79%" font="300">
+                <Caption left="88.5%" right="2.5%" bottom="78.5%" font="300">
                   <svg viewBox="0 0 60 36">
                     <text x="0" y="12" fill="#96FFFF">Japan</text>
                     <text x="5" y="32" fill="#96FFFF">2018</text>
                   </svg>
                 </Caption>
-                <Caption left="77.5%" right="16%" bottom="42%" font="300">
+                <Caption left="87%" right="4%" bottom="41%" font="300">
                   <svg viewBox="0 0 60 15">
                     <text x="0" y="12" fill="#96FFFF">Keep fit</text>
                   </svg>
                 </Caption>
-                <Caption left="75%" right="18.5%" bottom="11%" font="300">
+                <Caption left="83.5%" right="7%" bottom="10.5%" font="300">
                   <svg viewBox="0 0 60 36">
                     <text x="5" y="12" fill="#96FFFF">Visit</text>
                     <text x="0" y="32" fill="#96FFFF">Africa</text>
                   </svg>
                 </Caption>
-                <Caption left="32.5%" right="59.5%" bottom="63%" font="300">
+                <Caption left="26.5%" right="63%" bottom="63.5%" font="300">
                   <svg viewBox="0 0 60 15">
                     <text x="0" y="12" fill="#96FFFF">Music</text>
                   </svg>
                 </Caption>
-                <Caption left="28.5%" right="61.5%" bottom="20.5%" font="300">
+                <Caption left="21%" right="65%" bottom="20%" font="300">
                   <svg viewBox="0 0 80 20">
                     <text x="0" y="12" fill="#96FFFF">Inspiration</text>
                   </svg>
                 </Caption>
-                <Caption left="62.75%" right="30%" bottom="71%" font="300">
+                <Caption left="67.25%" right="22.25%" bottom="71%" font="300">
                   <svg viewBox="0 0 60 15">
                     <text x="0" y="12" fill="#96FFFF">Travel</text>
                   </svg>
                 </Caption>
-                <Caption left="62%" right="30.5%" bottom="36%" font="300">
+                <Caption left="65.5%" right="24%" bottom="36%" font="300">
                   <svg viewBox="0 0 60 15">
                     <text x="0" y="12" fill="#96FFFF">Dreams</text>
                   </svg>
                 </Caption>
-                <Caption left="43.75%" right="42.5%" bottom="52%" font="700">
+                <Caption left="40.75%" right="39.5%" bottom="52%" font="700">
                   <svg viewBox="0 0 100 21">
                     <text x="6" y="18" fill="#131C3E">NeuralNotes</text>
                   </svg>

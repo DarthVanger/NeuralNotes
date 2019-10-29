84fb4fcdcb4340ac221e048b0bac17a2a6cfdb84
TomaTomoe <denysiuk.tina@gmail.com>
2019-10-29 18:58:25 +0200

    revisions to comments on commit number 49ff63d

--- src/components/LoginPage/Slide2.js
+++ src/components/LoginPage/Slide2.js
@@ -5,9 +5,6 @@ import slideImage from 'components/LoginPage/images/slide-2.svg';
 const Slide = styled.section`
   padding-bottom: 10%;
   text-align: center;
-  Img {
-    width: 74%;
-  }
 `;
 const H3 = styled.h3`
   padding-bottom: 7%;
@@ -17,24 +14,16 @@ const H3 = styled.h3`
 `;
 const Figure = styled.figure`
   position: relative;
-  width: 100%;
+  width: 74%;
+  margin: auto;
   height: auto;
 `;
 const Img = styled.img`
   width: 100%;
   height: auto;
 `;
-const CaptionsConteiner = styled.div`
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 13%;
-  right: 13%;
-  padding: 0;
-  color: #ffffff;
-  text-align: center;
-`;
 const Caption = styled.div`
+  font-weight: ${props => props.font || 300};
   position: absolute;
   top: ${props => props.top};
   left: ${props => props.left};
@@ -43,6 +32,12 @@ const Caption = styled.div`
   font-weight: ${props => props.font};
 `;
 
+const CaptionSvgTextBox = ({width = 60, height = 15, ...rest}) => {
+  return <svg viewBox={`0 0 ${width} ${height}`} {...rest} />;
+};
+const CaptionSvgText = ({x = 0, y = 12, fill = "#96FFFF", text}) => {
+  return <text x={x} y={y} fill={fill}>{text}</text>;
+};
 
 export default class Slide2 extends Component {
     render() {
@@ -53,76 +48,74 @@ export default class Slide2 extends Component {
               As A Mind Map</H3>
             <Figure>
               <Img src={slideImage} />
-              <CaptionsConteiner>
-                <Caption left="14.5%" right="76%" bottom="84.5%" font="300">
-                  <svg viewBox="0 0 60 15">
-                    <text x="0" y="12" fill="#96FFFF">Chill</text>
-                  </svg>
-                </Caption>
-                <Caption left="4.5%" right="86%" bottom="60%" font="300">
-                  <svg viewBox="0 0 60 15">
-                    <text x="0" y="12" fill="#96FFFF">Rock</text>
-                  </svg>
-                </Caption>
-                <Caption left="4.5%" right="85%" bottom="24.5%" font="300">
-                  <svg viewBox="0 0 60 15">
-                    <text x="0" y="12" fill="#96FFFF">Art</text>
-                  </svg>
-                </Caption>
-                <Caption left="46.5%" right="44%" bottom="7.5%" font="300">
-                  <svg viewBox="0 0 60 15">
-                    <text x="0" y="12" fill="#96FFFF">Quotes</text>
-                  </svg>
-                </Caption>
-                <Caption left="50%" right="41%" bottom="85%" font="300">
-                  <svg viewBox="0 0 60 36">
-                    <text x="0" y="12" fill="#96FFFF">Berlin</text>
-                    <text x="2" y="32" fill="#96FFFF">2019</text>
-                  </svg>
-                </Caption>
-                <Caption left="88.5%" right="2.5%" bottom="78.5%" font="300">
-                  <svg viewBox="0 0 60 36">
-                    <text x="0" y="12" fill="#96FFFF">Japan</text>
-                    <text x="5" y="32" fill="#96FFFF">2018</text>
-                  </svg>
-                </Caption>
-                <Caption left="87%" right="4%" bottom="41%" font="300">
-                  <svg viewBox="0 0 60 15">
-                    <text x="0" y="12" fill="#96FFFF">Keep fit</text>
-                  </svg>
-                </Caption>
-                <Caption left="83.5%" right="7%" bottom="10.5%" font="300">
-                  <svg viewBox="0 0 60 36">
-                    <text x="5" y="12" fill="#96FFFF">Visit</text>
-                    <text x="0" y="32" fill="#96FFFF">Africa</text>
-                  </svg>
-                </Caption>
-                <Caption left="26.5%" right="63%" bottom="63.5%" font="300">
-                  <svg viewBox="0 0 60 15">
-                    <text x="0" y="12" fill="#96FFFF">Music</text>
-                  </svg>
-                </Caption>
-                <Caption left="21%" right="65%" bottom="20%" font="300">
-                  <svg viewBox="0 0 80 20">
-                    <text x="0" y="12" fill="#96FFFF">Inspiration</text>
-                  </svg>
-                </Caption>
-                <Caption left="67.25%" right="22.25%" bottom="71%" font="300">
-                  <svg viewBox="0 0 60 15">
-                    <text x="0" y="12" fill="#96FFFF">Travel</text>
-                  </svg>
-                </Caption>
-                <Caption left="65.5%" right="24%" bottom="36%" font="300">
-                  <svg viewBox="0 0 60 15">
-                    <text x="0" y="12" fill="#96FFFF">Dreams</text>
-                  </svg>
-                </Caption>
-                <Caption left="40.75%" right="39.5%" bottom="52%" font="700">
-                  <svg viewBox="0 0 100 21">
-                    <text x="6" y="18" fill="#131C3E">NeuralNotes</text>
-                  </svg>
-                </Caption>
-              </CaptionsConteiner>
+              <Caption left="14.5%" right="76%" bottom="84.5%">
+                <CaptionSvgTextBox>
+                  <CaptionSvgText text="Chill"/>
+                </CaptionSvgTextBox>
+              </Caption>
+              <Caption left="4.5%" right="86%" bottom="60%">
+                <CaptionSvgTextBox>
+                  <CaptionSvgText text="Rock"/>
+                </CaptionSvgTextBox>
+              </Caption>
+              <Caption left="4.5%" right="85%" bottom="24.5%">
+                <CaptionSvgTextBox>
+                  <CaptionSvgText text="Art"/>
+                </CaptionSvgTextBox>
+              </Caption>
+              <Caption left="46.5%" right="44%" bottom="7.5%">
+                <CaptionSvgTextBox>
+                  <CaptionSvgText text="Quotes"/>
+                </CaptionSvgTextBox>
+              </Caption>
+              <Caption left="50%" right="41%" bottom="85%">
+                <CaptionSvgTextBox height="36">
+                  <CaptionSvgText text="Berlin"/>
+                  <CaptionSvgText x="2" y="32" text="2019"/>
+                </CaptionSvgTextBox>
+              </Caption>
+              <Caption left="88.5%" right="2.5%" bottom="78.5%">
+                <CaptionSvgTextBox height="36">
+                  <CaptionSvgText text="Japan"/>
+                  <CaptionSvgText x="5" y="32" text="2018"/>
+                </CaptionSvgTextBox>
+              </Caption>
+              <Caption left="87%" right="4%" bottom="41%">
+                <CaptionSvgTextBox>
+                  <CaptionSvgText text="Keep fit"/>
+                </CaptionSvgTextBox>
+              </Caption>
+              <Caption left="83.5%" right="7%" bottom="10.5%">
+                <CaptionSvgTextBox height="36">
+                  <CaptionSvgText x="5" text="Visit"/>
+                  <CaptionSvgText y="32" text="Africa"/>
+                </CaptionSvgTextBox>
+              </Caption>
+              <Caption left="26.5%" right="63%" bottom="63.5%">
+                <CaptionSvgTextBox>
+                  <CaptionSvgText text="Music"/>
+                </CaptionSvgTextBox>
+              </Caption>
+              <Caption left="21%" right="65%" bottom="20%">
+                <CaptionSvgTextBox width="80" height="20">
+                  <CaptionSvgText text="Inspiration"/>
+                </CaptionSvgTextBox>
+              </Caption>
+              <Caption left="67.25%" right="22.25%" bottom="71%">
+                <CaptionSvgTextBox>
+                  <CaptionSvgText text="Travel"/>
+                </CaptionSvgTextBox>
+              </Caption>
+              <Caption left="65.5%" right="24%" bottom="36%">
+                <CaptionSvgTextBox>
+                  <CaptionSvgText text="Dreams"/>
+                </CaptionSvgTextBox>
+              </Caption>
+              <Caption left="40.75%" right="39.5%" bottom="52%" font="700">
+                <CaptionSvgTextBox width="100" height="21">
+                  <CaptionSvgText x="6" y="18" fill="#131C3E" text="NeuralNotes"/>
+                </CaptionSvgTextBox>
+              </Caption>
             </Figure>
           </Slide>
         );

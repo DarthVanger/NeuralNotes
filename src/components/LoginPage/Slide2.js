764c6e88ef7f7cd4025025a59a116cc950d67c8f
tomaTomoe <denysiuk.tina@gmail.com>
2019-11-14 16:12:36 +0200

    fix bugs with svg containers + corrections of some variable names for Slide2

--- src/components/LoginPage/Slide2.js
+++ src/components/LoginPage/Slide2.js
@@ -22,18 +22,18 @@ const Img = styled.img`
   width: 100%;
   height: auto;
 `;
-const Caption = styled.div`
-  font-weight: ${props => props.font || 300};
+const Caption = styled.svg`
+  font-weight: ${props => props.fontWeight || 300};
   position: absolute;
+  width: ${props => props.width};
   top: ${props => props.top};
   left: ${props => props.left};
   right: ${props => props.right};
   bottom: ${props => props.bottom};
-  font-weight: ${props => props.font};
 `;
 
-const CaptionSvgTextBox = ({width = 60, height = 15, ...rest}) => {
-  return <svg viewBox={`0 0 ${width} ${height}`} {...rest} />;
+const CaptionSvgTextBox = ({viewBoxWidth = 60, viewBoxHeight = 15, ...rest}) => {
+  return <Caption viewBox={`0 0 ${viewBoxWidth} ${viewBoxHeight}`} {...rest} />;
 };
 const CaptionSvgText = ({x = 0, y = 12, fill = "#96FFFF", text}) => {
   return <text x={x} y={y} fill={fill}>{text}</text>;
@@ -48,74 +48,48 @@ export default class Slide2 extends Component {
               As A Mind Map</H3>
             <Figure>
               <Img src={slideImage} />
-              <Caption left="14.5%" right="76%" bottom="84.5%">
-                <CaptionSvgTextBox>
+              <CaptionSvgTextBox left="14.5%" top="11%" width="9.5%">
                   <CaptionSvgText text="Chill"/>
                 </CaptionSvgTextBox>
-              </Caption>
-              <Caption left="4.5%" right="86%" bottom="60%">
-                <CaptionSvgTextBox>
+              <CaptionSvgTextBox left="4.5%" top="35%" width="9.5%">
                   <CaptionSvgText text="Rock"/>
                 </CaptionSvgTextBox>
-              </Caption>
-              <Caption left="4.5%" right="85%" bottom="24.5%">
-                <CaptionSvgTextBox>
+              <CaptionSvgTextBox left="4.5%" top="71%" width="9.5%">
                   <CaptionSvgText text="Art"/>
                 </CaptionSvgTextBox>
-              </Caption>
-              <Caption left="46.5%" right="44%" bottom="7.5%">
-                <CaptionSvgTextBox>
+              <CaptionSvgTextBox left="46.5%" top="88%" width="9.5%">
                   <CaptionSvgText text="Quotes"/>
                 </CaptionSvgTextBox>
-              </Caption>
-              <Caption left="50%" right="41%" bottom="85%">
-                <CaptionSvgTextBox height="36">
+              <CaptionSvgTextBox viewBoxHeight="36" left="50%" top="6%" width="9.5%">
                   <CaptionSvgText text="Berlin"/>
                   <CaptionSvgText x="2" y="32" text="2019"/>
                 </CaptionSvgTextBox>
-              </Caption>
-              <Caption left="88.5%" right="2.5%" bottom="78.5%">
-                <CaptionSvgTextBox height="36">
+              <CaptionSvgTextBox viewBoxHeight="36" left="88.5%" top="12%" width="9.5%">
                   <CaptionSvgText text="Japan"/>
                   <CaptionSvgText x="5" y="32" text="2018"/>
                 </CaptionSvgTextBox>
-              </Caption>
-              <Caption left="87%" right="4%" bottom="41%">
-                <CaptionSvgTextBox>
+              <CaptionSvgTextBox left="87%" top="54%" width="9.5%">
                   <CaptionSvgText text="Keep fit"/>
                 </CaptionSvgTextBox>
-              </Caption>
-              <Caption left="83.5%" right="7%" bottom="10.5%">
-                <CaptionSvgTextBox height="36">
+              <CaptionSvgTextBox viewBoxHeight="36" left="83.5%" top="80%" width="9.5%">
                   <CaptionSvgText x="5" text="Visit"/>
                   <CaptionSvgText y="32" text="Africa"/>
                 </CaptionSvgTextBox>
-              </Caption>
-              <Caption left="26.5%" right="63%" bottom="63.5%">
-                <CaptionSvgTextBox>
+              <CaptionSvgTextBox left="26.5%" top="31.5%" width="10.5%">
                   <CaptionSvgText text="Music"/>
                 </CaptionSvgTextBox>
-              </Caption>
-              <Caption left="21%" right="65%" bottom="20%">
-                <CaptionSvgTextBox width="80" height="20">
+              <CaptionSvgTextBox viewBoxWidth="80" viewBoxHeight="20" left="21%" top="73.5%" width="14%">
                   <CaptionSvgText text="Inspiration"/>
                 </CaptionSvgTextBox>
-              </Caption>
-              <Caption left="67.25%" right="22.25%" bottom="71%">
-                <CaptionSvgTextBox>
+              <CaptionSvgTextBox left="67.25%" top="25%" width="10.5%">
                   <CaptionSvgText text="Travel"/>
                 </CaptionSvgTextBox>
-              </Caption>
-              <Caption left="65.5%" right="24%" bottom="36%">
-                <CaptionSvgTextBox>
+                <CaptionSvgTextBox left="66%" top="58.5%" width="10.5%">
                   <CaptionSvgText text="Dreams"/>
                 </CaptionSvgTextBox>
-              </Caption>
-              <Caption left="40.75%" right="39.5%" bottom="52%" font="700">
-                <CaptionSvgTextBox width="100" height="21">
+              <CaptionSvgTextBox viewBoxWidth="100" viewBoxHeight="21" left="41.5%" top="41%" width="18.5%" fontWeight="700">
                   <CaptionSvgText x="6" y="18" fill="#131C3E" text="NeuralNotes"/>
                 </CaptionSvgTextBox>
-              </Caption>
             </Figure>
           </Slide>
         );

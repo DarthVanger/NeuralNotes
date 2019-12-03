03c5f6d943fc184d59ab4625f6129eb16746f55a
tomaTomoe <denysiuk.tina@gmail.com>
2019-12-03 14:31:45 +0200

    Slide5 placed in separated file. Added captions for this slide

--- src/components/LoginPage/LoginPageComponent.js
+++ src/components/LoginPage/LoginPageComponent.js
@@ -5,8 +5,7 @@ import Slide1 from 'components/LoginPage/Slide1';
 import Slide2 from 'components/LoginPage/Slide2';
 import Slide3 from 'components/LoginPage/Slide3';
 import Slide4 from 'components/LoginPage/Slide4';
-import arrow from 'components/LoginPage/images/arrow.svg'
-import slide5 from 'components/LoginPage/images/slide-5.svg';
+import Slide5 from 'components/LoginPage/Slide5';
 import iconGoogle from 'components/LoginPage/images/icon-google.svg';
 
 const Main = styled.main`
@@ -21,16 +20,6 @@ const Section = styled.section`
   margin: auto;
 `;
 
-const Figure = styled.figure`
-  width: 100%;
-  height: auto;
-`;
-
-const Img = styled.img`
-  width: 100%;
-  height: auto;
-`;
-
 const H3 = styled.h3`
   padding-bottom: 7%;
   font-size: calc(13px + (24 - 13)*(100vw - 375px)/(1200 - 375));
@@ -38,78 +27,9 @@ const H3 = styled.h3`
   color: #ffffff;
 `;
 
-const ArrowUp = styled.img`
-  width: 2%;
-  height: auto;
-`;
-
-const ArrowDown = styled.img`
-  width: 2%;
-  height: auto;
-  transform: rotate(180deg);
-`;
-
-const Tooltip = styled.div`
-  width: 29%;
-  background: #0E1736;
-  border-radius: 5px;
-  box-shadow: inset 0px 4px 2px rgba(0, 0, 0, 0.25);
-  padding-left: 2.7%;
-  padding-right: 2.7%;
-  padding-top: 2.4%;
-  padding-bottom: 2.4%;
-  color: #E8E9F8;
-  text-transform: uppercase;
-  font-weight: 700;
-  margin: 1vw;
-`;
-
 const Button = styled.a`
   display: block;
 
-`;
-
-const Container = styled.div`
-  position: relative;
-`;
-
-const Slide5 = styled.section`
-  position: relative;
-
-  H3 {
-    padding-bottom: 3%;
-  }
-  .tooltip-right {
-    position: absolute;
-    right: 0;
-    font-size: 0.7rem;
-    text-align: center;
-    padding-top: 1.4%;
-    padding-bottom: 0.4%;
-
-    img {
-      width: 12%;
-    }
-  }
-  .tooltip-left {
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    font-size: 0.7rem;
-    text-align: center;
-    padding-top: 0.4%;
-    padding-bottom: 1.4%;
-    img {
-      width: 12%;
-    }
-  }
-
-  Figure {
-    Img {
-      padding-right: 8%;
-      padding-bottom: 5%;
-    }
-  }
 `;
 
 const Footer = styled.footer`
@@ -137,33 +57,7 @@ export class LoginPageComponent extends Component {
           <Slide2 />
           <Slide3 />
           <Slide4 />
-          <Slide5>
-            <H3>Frequently used notes
-              <br />
-              get bigger
-            </H3>
-            <Container>
-            <Tooltip className='tooltip-right'>
-              OLd unused notes
-                <br/>
-                are 
-                <span> small</span>
-                <br />
-                <ArrowDown src={arrow} />
-            </Tooltip>
-            <Figure>
-              <Img src={slide5} />
-            </Figure>
-            <Tooltip className='tooltip-left'>
-                <ArrowUp src={arrow} />
-                <br />
-              Recent, frequently
-                <br />
-                used notes are 
-                <span> big</span>
-            </Tooltip>
-            </Container>
-          </Slide5>
+          <Slide5 />
           <Footer>
             <H3><span>Neural</span>Notes is open source and free</H3>
             <Button onClick={authHandler}><span><img src={iconGoogle} /></span>Sing up with Google</Button>
---
+++ src/components/LoginPage/Slide5.js
@@ -0,0 +1,140 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import slideImage from 'components/LoginPage/images/slide-5.svg';
+import arrow from 'components/LoginPage/images/arrow.svg';
+
+const Slide = styled.section`
+  position: relative;
+   .shadow {
+    -webkit-filter: drop-shadow( 0px 7px 4px rgba(0, 0, 0, .3));
+    filter: drop-shadow( 0px 7px 4px rgba(0, 0, 0, .3));
+    }
+  .hint-old-notes {
+    position: absolute;
+    right: 5%;
+    width: 22%;
+    height: 11%;
+    top: 22%;
+    img {
+      width: 12%;
+      position: absolute;
+      bottom: 2%;
+      left: 44%;
+    }
+  }
+  .hint-resent-notes {
+    position: absolute;
+    left: 13%;
+    bottom: 3%;
+    width: 26%;
+    height: 13%;
+    img {
+      width: 12%;
+      position: absolute;
+      top: 2%;
+      left: 44%;
+    }
+  }
+`;
+const H3 = styled.h3`
+  padding-bottom: 0;
+  margin-top: 0;
+  margin-bottom: 0;
+  font-size: 4vmin;
+  text-align: center;
+  color: #ffffff;
+`;
+const Figure = styled.figure`
+  width: 100%;
+  height: auto;
+  position: relative;
+`;
+const Img = styled.img`
+  width: 100%;
+  height: auto;
+`;
+const ArrowUp = styled.img`
+  width: 2%;
+  height: auto;
+`;
+const ArrowDown = styled.img`
+  width: 2%;
+  height: auto;
+  transform: rotate(180deg);
+`;
+const Hint = styled.div`
+  background: #0E1736;
+  border-radius: 1vw;
+  box-shadow: inset 0px 4px 2px rgba(0, 0, 0, 0.25);
+`;
+const Caption = styled.svg`
+  font-weight: ${props => props.fontWeight || 300};
+  position: absolute;
+  width: ${props => props.width};
+  top: ${props => props.top};
+  left: ${props => props.left};
+  right: ${props => props.right};
+  bottom: ${props => props.bottom};
+`;
+
+const CaptionSvgTextBox = ({ viewBoxWidth = 60, viewBoxHeight = 40, ...rest }) => {
+    return <Caption viewBox={`0 0 ${viewBoxWidth} ${viewBoxHeight}`} {...rest} />;
+};
+const CaptionSvgText = ({ x = 0, y = 12, fill = "#96FFFF", text }) => {
+    return <text x={x} y={y} fill={fill}>{text}</text>;
+};
+
+export default class Slide5 extends Component {
+    render() {
+      return (
+        <Slide>
+            <H3>Frequently used notes<br />get bigger</H3>
+            <Hint className='hint-old-notes'>
+              <CaptionSvgTextBox viewBoxWidth="160" viewBoxHeight="40" left="3%" top="10%" width="94%" fontWeight="700">
+                <CaptionSvgText x="6" y="18" fill="#E8E9F8" text="OLD UNUSED NOTES" />
+                <CaptionSvgText x="36" y="38" fill="#E8E9F8" text="ARE" />
+                <CaptionSvgText x="71" y="38" fill="#96FFFF" text="SMALL" />
+              </CaptionSvgTextBox>
+              <ArrowDown src={arrow} />
+            </Hint>
+            <Figure>
+              <Img src={slideImage} className='shadow' />
+              <CaptionSvgTextBox viewBoxWidth="100" viewBoxHeight="21" left="34%" top="21%" width="15.5%" fontWeight="700">
+                <CaptionSvgText x="6" y="18" fill="#131C3E" text="NeuralNotes" />
+              </CaptionSvgTextBox>
+              <CaptionSvgTextBox viewBoxWidth="45" left="52.5%" top="48%" width="9.5%" fontWeight="700">
+                <CaptionSvgText y="16" text="Work" />
+              </CaptionSvgTextBox>
+              <CaptionSvgTextBox left="19.5%" top="51%" width="13.5%" fontWeight="700">
+                <CaptionSvgText y="16" text="Current" />
+                <CaptionSvgText y="32" text="Project" />
+              </CaptionSvgTextBox>
+              <CaptionSvgTextBox left="14.5%" top="26%" width="7.5%">
+                <CaptionSvgText y="14" text="Personal" />
+                <CaptionSvgText x="20" y="32" text="life" />
+              </CaptionSvgTextBox>
+              <CaptionSvgTextBox left="61%" top="25.5%" width="7.5%">
+                <CaptionSvgText x="12" y="14" text="Old" />
+                <CaptionSvgText y="32" text="project" />
+              </CaptionSvgTextBox>
+              <CaptionSvgTextBox viewBoxWidth="70" left="81%" top="35%" width="8%">
+                <CaptionSvgText y="14" text="Forgotten" />
+                <CaptionSvgText x="10" y="32" text="project" />
+              </CaptionSvgTextBox>
+              <CaptionSvgTextBox viewBoxWidth="82" left="79%" top="60%" width="10%">
+                <CaptionSvgText y="14" text="Abandoned" />
+                <CaptionSvgText x="17" y="32" text="project" />
+              </CaptionSvgTextBox>
+            </Figure>
+          <Hint className='hint-resent-notes'>
+            <ArrowUp src={arrow} />
+            <CaptionSvgTextBox viewBoxWidth="170" viewBoxHeight="40" left="7%" top="35%" width="86%" fontWeight="700">
+              <CaptionSvgText x="3" y="18" fill="#E8E9F8" text="RECENT, FREQUENTLY" />
+              <CaptionSvgText x="3" y="38" fill="#E8E9F8" text="USED NOTES ARE" />
+              <CaptionSvgText x="137" y="38" fill="#96FFFF" text="BIG" />
+            </CaptionSvgTextBox>
+          </Hint>
+        </Slide>
+      );
+    }
+};

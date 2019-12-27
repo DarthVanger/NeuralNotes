fc05f67e05d1ad52e41050fb49a8948ff0b094d7
DarthVanger <darthvanger@gmail.com>
2019-12-27 21:58:40 +0200

    Re-render mind map when note parent changed
    re #24

--- src/components/NotesMindMapView/NotesMindMapViewContainer.js
+++ src/components/NotesMindMapView/NotesMindMapViewContainer.js
@@ -5,11 +5,11 @@ import {
   CHANGE_SELECTED_NOTE_ACTION,
   NOTE_CHANGE_PARENT_ACTION,
   DELETE_NOTE_ACTION,
-  SWITCH_CHANGE_PARENT_MODE_ACTION,
   CREATE_EMPTY_CHILD_ACTION,
   EDIT_NOTE_NAME_ACTION,
   UPDATE_NOTE_NAME_ACTION,
   MIND_MAP_CLICKED_ACTION,
+  CHANGE_PARENT_BUTTON_CLICKED_ACTION,
 } from 'components/NotesMindMapView/NotesMindMapViewActions';
 import { NotesMindMapViewComponent } from 'components/NotesMindMapView/NotesMindMapViewComponent';
 
@@ -26,12 +26,12 @@ const mapStateToProps = ({ notesMindMap: { rootNote, selectedNote, noteText, sho
 const mapDispatchToProps = () => ({
   changeSelectedNote: data => action(CHANGE_SELECTED_NOTE_ACTION, data),
   changeParentNote: data => action(NOTE_CHANGE_PARENT_ACTION, data),
-  switchChangeParentMode: data => action(SWITCH_CHANGE_PARENT_MODE_ACTION, data),
   createEmptyChild: data => action(CREATE_EMPTY_CHILD_ACTION, data),
   deleteNote: data => action(DELETE_NOTE_ACTION, data),
   editNote: data => action(EDIT_NOTE_NAME_ACTION, data),
   updateNoteName: data => action(UPDATE_NOTE_NAME_ACTION, data),
   onMindMapClick: data => action(MIND_MAP_CLICKED_ACTION, data),
+  onChangeParentButtonClick: data => action(CHANGE_PARENT_BUTTON_CLICKED_ACTION, data),
 });
 
 export const NotesMindMapViewContainer = connect(

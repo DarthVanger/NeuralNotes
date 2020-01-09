5bc2302492c89c73d95890420aa7a4deccba047d
DarthVanger <darthvanger@gmail.com>
2020-01-09 22:09:41 +0200

    Stop asking google drive for access to all files
    Ask google drive for access only for files created and
    opened with the app.
    Remove asking for permission to read metadata of all
    files on user's Google Drive
    
    fix #66

--- src/api/google-client-config.js
+++ src/api/google-client-config.js
@@ -6,7 +6,6 @@ export const apiKey = 'AIzaSyAPXuniw1OFvl6OgeIuZp3NSbqfrjnw8qA';
 export const scopes = [
   // Per-file access to files created or opened by the app
   'https://www.googleapis.com/auth/drive.file',
-  'https://www.googleapis.com/auth/drive.metadata.readonly'
 ];
 
 export const discoveryDocs = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];

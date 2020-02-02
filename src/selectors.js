9cb3888dc7188ae7b9ce1277b6f46bf79531d5d8
Dmytro Salenko <salenko.d@pm.me>
2020-02-02 19:01:37 +0200

    update doc

--- src/selectors.js
+++ src/selectors.js
@@ -7,6 +7,7 @@ import * as UploadsSelectors from './components/Uploads/UploadsSelectors';
  * Note!!!
  * Don't import it to local reducer level selectors to avoid circular dependencies
  *
+ * Import them when you want to use them in the components where you want to use selectors from different modules
  * https://cmichel.io/redux-selectors-structure
  */
 

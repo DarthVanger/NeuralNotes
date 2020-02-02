e76b812f5205ca75c80a85b6aacb24585ea20921
Dmytro Salenko <salenko.d@pm.me>
2020-02-02 18:55:33 +0200

    removed withPayload helper

--- src/components/Uploads/UploadsActions.js
+++ src/components/Uploads/UploadsActions.js
@@ -1,20 +1,19 @@
 import { createActions } from 'redux-actions';
-import { withPayload } from 'helpers/redux';
 import { UPLOADS_REDUCER_KEY } from './UploadsConstants';
 
 export const UploadsActions = createActions(
   {
     LIST: {
-      ADDED_FILES: withPayload('files'),
+      ADDED_FILES: files => ({ files }),
       CLEAR: null,
     },
     FILE: {
-      SESSION_RETRIEVED: withPayload('file', 'session'),
-      PROGRESS_UPDATED: withPayload('file', 'progress'),
-      UPLOAD_SUCCESS: withPayload('file', 'result'),
-      UPLOAD_FAILURE: withPayload('file', 'error'),
-      CANCEL_UPLOAD: withPayload('file'),
-      RETRY_UPLOAD: withPayload('file', { error: null }),
+      SESSION_RETRIEVED: (file, session) => ({ file, session }),
+      PROGRESS_UPDATED: (file, progress) => ({ file, progress }),
+      UPLOAD_SUCCESS: (file, result) => ({ file, result }),
+      UPLOAD_FAILURE: (file, error) => ({ file, error }),
+      CANCEL_UPLOAD: file => ({ file }),
+      RETRY_UPLOAD: file => ({ file, error: null }),
     },
   },
   {
--- src/helpers/redux.js
+++ src/helpers/redux.js
@@ -1,36 +1,6 @@
 import dotProp from 'dot-prop-immutable';
 
 /**
- * Creates a function which transforms arg list to objekt with followed keys
- *
- * Example:
- *
- * const changeParentPayloadCreator = withPayload('nodeId', 'nextParentId');
- * const payload = changeParentPayloadCreator(1001, 2000);
- * // payload = {
- * //   nodeId: 1001,
- * //   nextParentId: 2000,
- * // }
- *
- * You can pass also an object in array if you want to pass some values to payload
- */
-export const withPayload = (...keys) => {
-  const createPayload = (...args) => {
-    return keys.reduce((payload, property, index) => {
-      if (typeof property === 'object') {
-        Object.assign(payload, property);
-      } else {
-        payload[property] = args[index];
-      }
-
-      return payload;
-    }, {});
-  };
-
-  return createPayload;
-};
-
-/**
  * Shorthand to update substate
  *
  * @param {*} name - property name to update

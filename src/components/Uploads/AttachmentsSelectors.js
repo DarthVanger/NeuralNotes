d1468634386da233fabfedd7bc0b07ee0734c0b9
Dmytro Salenko <salenko.d@pm.me>
2020-02-02 09:36:04 +0200

    clean up

--- src/components/Uploads/AttachmentsSelectors.js
+++ src/components/Uploads/AttachmentsSelectors.js
@@ -1,21 +1,11 @@
 import { createSelector } from 'reselect';
 import { UPLOADS_REDUCER_KEY } from './UploadsConstants';
-import { NotesMindMapSelectors } from './AttachmentsImportedSelectors';
-
-export const isUploadButtonVisible =
-  NotesMindMapSelectors.isSelectedNoteRealNote;
-
-export const getUploadFolderId = NotesMindMapSelectors.getSelectedNoteId;
 
 const getAttachmentsState = state => state[UPLOADS_REDUCER_KEY];
 
 export const getUploads = createSelector(
   getAttachmentsState,
   attachments => attachments.list,
-);
-
-export const getUploadFiles = createSelector(getUploads, uploads =>
-  uploads.map(item => item.file),
 );
 
 export const hasUploadingFiles = createSelector(

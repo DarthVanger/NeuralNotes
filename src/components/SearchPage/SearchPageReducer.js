6dc46f8e7afae319866f9d1ac614eae5913ea311
Jonathan Palacio <jpalacio0612@gmail.com>
2020-10-01 08:29:33 -0500

    added SearchPageSagas.js

--- src/components/SearchPage/SearchPageReducer.js
+++ src/components/SearchPage/SearchPageReducer.js
@@ -1,14 +1,19 @@
-import { SEARCH_QUERY_CHANGED_ACTION } from 'components/SearchPage/SearchPageAction';
+import {
+  SEARCH_QUERY_CHANGED_ACTION,
+  SEARCH_REQUEST_SUCCESS,
+} from 'components/SearchPage/SearchPageAction';
 
 const defaultState = {
   query: '',
+  results: [],
 };
 
 export const searchPageReducer = (state = defaultState, { type, data }) => {
   switch (type) {
     case SEARCH_QUERY_CHANGED_ACTION:
       return { ...state, query: data };
-
+    case SEARCH_REQUEST_SUCCESS:
+      return { ...state, results: data };
     default:
       return state;
   }

e3166a07a0e47b9ffc5abc9c62576f5a4b40891c
DarthVanger <darthvanger@gmail.com>
2020-10-03 19:40:57 +0300

    Add theme colors for material ui
    related to #144

--- src/colors.js
+++ src/colors.js
@@ -2,6 +2,8 @@ export const colors = {
   lightGray: '#888',
   darkGray: '#333',
   darkViolet: '#2B2630',
+  violet: '#BB86FC',
+  turquoise: '#88EDE7',
   white87: 'rgba(255, 255, 255, 0.87)',
   white60: 'rgba(255, 255, 255, 0.6)',
 };
--- src/components/App/AppComponent.js
+++ src/components/App/AppComponent.js
@@ -1,11 +1,25 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import { colors } from '../../colors';
 
 import { NotesPageContainer } from 'components/NotesPage/NotesPageContainer';
 import { PAGES_ENUM } from 'components/App/AppConstants';
 import { LoginPageContainer } from 'components/LoginPage/LoginPageContainer';
 import { UploadsPage } from 'components/Uploads/UploadsPage';
 import { SearchPageComponent } from 'components/SearchPage/SearchPageComponent';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: colors.violet,
+    },
+    secondary: {
+      main: colors.turquoise,
+    },
+  },
+});
 
 export class AppComponent extends PureComponent {
   render() {
@@ -21,7 +35,11 @@ export class AppComponent extends PureComponent {
         return <NotesPageContainer />;
 
       case PAGES_ENUM.UPLOADS:
-        return <UploadsPage />;
+        return (
+          <ThemeProvider theme={theme}>
+            <UploadsPage />
+          </ThemeProvider>
+        );
 
       case PAGES_ENUM.SEARCH:
         return <SearchPageComponent />;

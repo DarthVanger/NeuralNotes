3840cad13b454cff3f7ba99dc75383d91ebecc93
kvoncode <kvoncode@gmail.com>
2020-10-18 17:56:44 +0900

    comment: unused code to conform eslint

--- src/components/NoteDetailsPage/NoteDetailsPage.js
+++ src/components/NoteDetailsPage/NoteDetailsPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useDispatch } from 'react-redux';
-import { PAGES_ENUM } from 'components/App/AppConstants';
-import { CHANGE_PAGE_ACTION } from 'components/App/AppActions';
+// import { useDispatch } from 'react-redux';
+// import { PAGES_ENUM } from 'components/App/AppConstants';
+// import { CHANGE_PAGE_ACTION } from 'components/App/AppActions';
 import { NoteDetailsContainer } from 'components/NoteDetails/NoteDetailsContainer';
 
 const FlexContainer = styled.div`
@@ -15,21 +15,21 @@ const FlexContainer = styled.div`
   bottom: 5rem;
 `;
 
-const StyledButton = styled.button`
-  width: 10rem;
-  height: 2rem;
-  color: #bb86fc;
-  background-color: #232323;
-  border: 1px solid #bb86fc;
-  font-size: 1rem;
-`;
+// const StyledButton = styled.button`
+//   width: 10rem;
+//   height: 2rem;
+//   color: #bb86fc;
+//   background-color: #232323;
+//   border: 1px solid #bb86fc;
+//   font-size: 1rem;
+// `;
 
 export const NoteDetailsPage = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const handleClick = () => {
-    dispatch({ type: CHANGE_PAGE_ACTION, data: PAGES_ENUM.NOTE_DETAILS });
-  };
+  // const handleClick = () => {
+  //   dispatch({ type: CHANGE_PAGE_ACTION, data: PAGES_ENUM.NOTE_DETAILS });
+  // };
   return (
     <>
       <NoteDetailsContainer />

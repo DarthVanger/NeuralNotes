26021d7f68a65e16aec691c4605da612ea0eb3a0
kvoncode <kvoncode@gmail.com>
2020-10-19 12:47:41 +0900

    add: onclick goback button go to notes tree page

--- src/components/NoteDetailsPage/NoteDetailsPage.js
+++ src/components/NoteDetailsPage/NoteDetailsPage.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-// import { useDispatch } from 'react-redux';
-// import { PAGES_ENUM } from 'components/App/AppConstants';
-// import { CHANGE_PAGE_ACTION } from 'components/App/AppActions';
+import { useDispatch } from 'react-redux';
+import { PAGES_ENUM } from 'components/App/AppConstants';
+import { CHANGE_PAGE_ACTION } from 'components/App/AppActions';
 import { NoteDetailsContainer } from 'components/NoteDetails/NoteDetailsContainer';
 
 const Wrapper = styled.div`
@@ -17,15 +17,15 @@ const BackButtonWrapper = styled.div`
 `;
 
 export const NoteDetailsPage = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  // const handleClick = () => {
-  //   dispatch({ type: CHANGE_PAGE_ACTION, data: PAGES_ENUM.NOTE_DETAILS });
-  // };
+  const handleClick = () => {
+    dispatch({ type: CHANGE_PAGE_ACTION, data: PAGES_ENUM.NOTES });
+  };
   return (
     <Wrapper>
       <BackButtonWrapper>
-        <IconButton aria-label="back">
+        <IconButton onClick={handleClick} aria-label="back">
           <ArrowBackIcon style={{ fill: '#E4E3E4' }} />
         </IconButton>
       </BackButtonWrapper>

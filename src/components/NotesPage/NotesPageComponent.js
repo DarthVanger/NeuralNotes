6967ca2b90b39a27c806ecd654b07cdd74c8d7a7
kvoncode <kvoncode@gmail.com>
2020-10-23 16:49:19 +0900

    replace: UploadButton with UploadFileButton

--- src/components/NotesPage/NotesPageComponent.js
+++ src/components/NotesPage/NotesPageComponent.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import { NotesMindMapContainer } from 'components/NotesMindMap/NotesMindMapContainer'; // Notes tree view
 import { TopBarComponent } from 'components/TopBar/TopBarComponent';
 import { UploadStatusButton } from 'components/Uploads/UploadStatusButton';
-import { UploadButton } from 'components/Uploads/UploadButton';
+// import { UploadButton } from 'components/Uploads/UploadButton';
 import BottomBarComponent from 'components/BottomBar/BottomBarComponent';
 
 export class NotesPageComponent extends Component {
@@ -18,7 +18,7 @@ export class NotesPageComponent extends Component {
         <NotesMindMapContainer />
         {/* {!isHelpViewed && <ControlsHelpComponent onClose={closeHelp} />} */}
         <UploadStatusButton />
-        <UploadButton />
+        {/* <UploadButton /> */}
         <BottomBarComponent />
       </>
     );

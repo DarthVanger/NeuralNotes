f256074b7c49d6bc888c7facc834ad8e9f5ff6ed
kvoncode <kvoncode@gmail.com>
2020-10-31 12:48:22 +0900

    move: gapi load() to appInit

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import * as ReactDOM from 'react-dom';
 import 'abortcontroller-polyfill/dist/polyfill-patch-fetch';
-
 import 'vis-network/dist/vis-network.css';
 import 'react-toastify/dist/ReactToastify.css';
 import 'sagas';
 import { App } from 'components/App/App';
 import { store, history } from 'sagas';
 import { GlobalStyle } from 'globalStyles';
-
 import { ConnectedRouter } from 'connected-react-router';
+
+// The app flow is following:
+// 1) The global code is executed. So it you have for example console.log() at the root of file it will execute
+// 2) redux-saga is imported before React, so saga's run() function will execute, and all effects will run in parallel
+// 3) The code inside React components will execute
+// 4) Saga's effects finish executing
 
 ReactDOM.render(
   <Provider store={store}>

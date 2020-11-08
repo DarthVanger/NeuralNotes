f8db51ea23af87ea4611411e7b4cbb3ad8c4e6e4
Anastasiia Salenko <batsvinova@gmail.com>
2020-11-08 12:01:12 +0200

    Added drop down

--- src/components/BottomBar/BottomBar.js
+++ src/components/BottomBar/BottomBar.js
@@ -3,10 +3,14 @@ import React from 'react';
 import { Typography } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Fab from '@material-ui/core/Fab';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import AddIcon from '@material-ui/icons/Add';
 import EditIcon from '@material-ui/icons/Edit';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
@@ -23,6 +27,12 @@ const useStyles = makeStyles(() => ({
   left: {
     position: 'absolute',
     color: colors.iconColor,
+    marginLeft: '30px',
+  },
+  moreIcon: {
+    position: 'absolute',
+    color: colors.iconColor,
+    padding: 0,
   },
   fabButton: {
     position: 'absolute',
@@ -37,14 +47,67 @@ const StyledLabel = styled.div`
   text-align: center;
 `;
 
+const options = ['Edit', 'Cange parent', 'Delete'];
+
+const ITEM_HEIGHT = 38;
+
 export const BottomBar = () => {
   const classes = useStyles();
 
   const selectedNote = useSelector(state => state.notesMindMap.selectedNote);
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const getMenuItems = () => {
+    if (!selectedNote.isNote) {
+      return options.slice(1);
+    }
+
+    return options;
+  };
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="fixed" className={classes.appBar}>
       <Toolbar>
+        <IconButton
+          className={classes.moreIcon}
+          aria-label="more"
+          aria-controls="long-menu"
+          aria-haspopup="true"
+          onClick={handleClick}>
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          id="long-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={open}
+          onClose={handleClose}
+          PaperProps={{
+            style: {
+              maxHeight: ITEM_HEIGHT * 4.5,
+              width: '168px',
+            },
+          }}>
+          {getMenuItems().map(option => (
+            <MenuItem
+              key={option}
+              selected={option === 'Edit'}
+              onClick={handleClose}>
+              <Typography variant="subtitle1">{option}</Typography>
+            </MenuItem>
+          ))}
+        </Menu>
+
         {selectedNote.isNote && (
           <>
             <Link to="/note" className={classes.left}>

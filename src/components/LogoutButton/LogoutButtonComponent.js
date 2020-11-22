ca7f900065d0f83d799f3a5cfca6f6bc4c776673
DarthVanger <darthvanger@gmail.com>
2020-11-22 15:01:33 +0200

    Remove commented code from LogoutButtonComponent

--- src/components/LogoutButton/LogoutButtonComponent.js
+++ src/components/LogoutButton/LogoutButtonComponent.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-// import { StyledLogoutButton } from 'components/LogoutButton/LogoutButtonStyles';
 import Button from '@material-ui/core/Button';
 
 export class LogoutButtonComponent extends Component {

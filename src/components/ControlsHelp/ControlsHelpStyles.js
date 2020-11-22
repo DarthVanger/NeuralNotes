535ac6a34e03bd0d18ff00aef04ac6e68b3ff1c0
DarthVanger <darthvanger@gmail.com>
2020-11-22 16:47:05 +0200

    Add back the ControlsHelp

---
+++ src/components/ControlsHelp/ControlsHelpStyles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const StyledTutorial = styled.div`
+  position: absolute;
+  bottom: 100px;
+  left: 0;
+  height: 5em;
+  padding: 0.5em;
+  background-color: yellow;
+`;
+
+export const StyledCrossButton = styled.span`
+  float: right;
+  cursor: pointer;
+`;
--- src/components/NotesPage/NotesPageComponent.js
+++ src/components/NotesPage/NotesPageComponent.js
@@ -3,16 +3,19 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { BottomBar } from 'components/BottomBar/BottomBar';
+import { ControlsHelpComponent } from 'components/ControlsHelp/ControlsHelpComponent';
 import { NotesMindMapContainer } from 'components/NotesMindMap/NotesMindMapContainer';
 import { TopBar } from 'components/TopBar/TopBar';
 
 export class NotesPageComponent extends Component {
   render() {
+    const { isHelpViewed, closeHelp } = this.props;
     return (
       <>
         <TopBar />
         <NotesMindMapContainer />
         <BottomBar />
+        {!isHelpViewed && <ControlsHelpComponent onClose={closeHelp} />}
       </>
     );
   }

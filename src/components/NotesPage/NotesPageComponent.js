11ae860692220c413bb4c1f65c21b1f35a87e4b7
DarthVanger <darthvanger@gmail.com>
2020-11-22 14:55:09 +0200

    Remove commented code from NotesPageComponent.js

--- src/components/NotesPage/NotesPageComponent.js
+++ src/components/NotesPage/NotesPageComponent.js
@@ -2,23 +2,16 @@ import React, { Component } from 'react';
 
 import PropTypes from 'prop-types';
 
-// import { ControlsHelpComponent } from 'components/ControlsHelp/ControlsHelpComponent';
 import { BottomBar } from 'components/BottomBar/BottomBar';
-import { NotesMindMapContainer } from 'components/NotesMindMap/NotesMindMapContainer'; // Notes tree view
+import { NotesMindMapContainer } from 'components/NotesMindMap/NotesMindMapContainer';
 import { TopBar } from 'components/TopBar/TopBar';
-
-// import { UploadButton } from 'components/Uploads/UploadButton';
 
 export class NotesPageComponent extends Component {
   render() {
-    // const { isHelpViewed, closeHelp } = this.props;
-
     return (
       <>
         <TopBar />
         <NotesMindMapContainer />
-        {/* {!isHelpViewed && <ControlsHelpComponent onClose={closeHelp} />} */}
-        {/* <UploadButton /> */}
         <BottomBar />
       </>
     );

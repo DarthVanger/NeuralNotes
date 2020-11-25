2c2020fa9194fa3640129ad58e6890694ed062ad
Anastasiia Salenko <batsvinova@gmail.com>
2020-11-25 21:52:36 +0200

    Fixes after review
    issue #178

--- src/components/BottomBar/BottomBar.js
+++ src/components/BottomBar/BottomBar.js
@@ -3,14 +3,10 @@ import React from 'react';
 import { Typography } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Fab from '@material-ui/core/Fab';
-import IconButton from '@material-ui/core/IconButton';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import AddIcon from '@material-ui/icons/Add';
 import EditIcon from '@material-ui/icons/Edit';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
@@ -18,6 +14,8 @@ import styled from 'styled-components';
 import { UploadButton } from 'components/Uploads/UploadButton';
 
 import { colors } from '../../colors';
+
+import { BottomBarMenu } from './BottomBarMenu';
 
 const useStyles = makeStyles(() => ({
   appBar: {
@@ -28,12 +26,6 @@ const useStyles = makeStyles(() => ({
     position: 'absolute',
     color: colors.iconColor,
     left: '46px',
-  },
-  moreIcon: {
-    position: 'absolute',
-    color: colors.iconColor,
-    left: 0,
-    padding: '12px',
   },
   fabButton: {
     position: 'absolute',
@@ -53,62 +45,10 @@ export const BottomBar = () => {
 
   const selectedNote = useSelector(state => state.notesMindMap.selectedNote);
 
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-
-  const handleClick = event => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
   return (
     <AppBar position="fixed" className={classes.appBar}>
       <Toolbar>
-        <IconButton
-          className={classes.moreIcon}
-          aria-label="more"
-          aria-haspopup="true"
-          onClick={handleClick}>
-          <MoreVertIcon />
-        </IconButton>
-        <Menu
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'left',
-          }}
-          transformOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left',
-          }}
-          anchorEl={anchorEl}
-          keepMounted
-          open={open}
-          onClose={handleClose}
-          PaperProps={{
-            style: {
-              width: '168px',
-            },
-          }}>
-          {selectedNote.isNote && (
-            <>
-              <Link to="/note" style={{ textDecoration: 'none' }}>
-                <MenuItem>
-                  <Typography variant="subtitle1">Edit</Typography>
-                </MenuItem>
-              </Link>
-            </>
-          )}
-          <MenuItem>
-            <Typography variant="subtitle1">Change parent</Typography>
-          </MenuItem>
-          <MenuItem>
-            <Typography variant="subtitle1">Delete</Typography>
-          </MenuItem>
-        </Menu>
-
+        <BottomBarMenu />
         {selectedNote.isNote && (
           <>
             <Link to="/note" className={classes.left}>

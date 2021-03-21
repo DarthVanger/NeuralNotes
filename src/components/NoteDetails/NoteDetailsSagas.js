31210992fba00d839c47352f441a90711fe09fb0
DarthVanger <darthvanger@gmail.com>
2021-03-21 18:32:32 +0200

    Remove spinner from NoteDetailsSagas

--- src/components/NoteDetails/NoteDetailsSagas.js
+++ src/components/NoteDetails/NoteDetailsSagas.js
@@ -1,8 +1,6 @@
 import { toast } from 'react-toastify';
 import { all, takeEvery, put, call } from 'redux-saga/effects';
 import noteStorage from 'storage/noteStorage';
-import siteGlobalLoadingBar from 'ui/spinner/site-global-loading-bar';
-
 import { EDIT_NOTE_BUTTON_CLICKED_ACTION } from 'components/BottomBar/BottomBarActions';
 import {
   noteNameUpdateRequestSuccessAction,
@@ -16,8 +14,6 @@ import {
   EDITOR_NOTE_CONTENT_CHANGED_ACTION,
 } from './NoteDetailsActions';
 
-const spinner = siteGlobalLoadingBar.create('note text editor');
-
 function* updateNoteName({ data: { note, newNoteName } }) {
   const newNote = yield noteStorage.updateNoteName({
     note,
@@ -28,18 +24,10 @@ function* updateNoteName({ data: { note, newNoteName } }) {
 
 function* updateNoteContent({ data: { note, noteContent } }) {
   let newNote = { ...note, content: noteContent };
-  let savingNoteContentSpinner = spinner.create('saving note content');
-  savingNoteContentSpinner.show();
-
-  console.debug(
-    'RealtimeSaving: Save note content: currentViewedNote: ',
-    newNote,
-  );
 
   try {
     const returnedNote = yield noteStorage.update(newNote);
     yield put(noteContentUpdateRequestSuccessAction(returnedNote));
-    savingNoteContentSpinner.hide();
   } catch (error) {
     toast.error('Failed to save note content');
     console.error(error);

adeac5fa12cbfee8101aa5273ee1eadbd5db218a
Oleg Ganchev <og77@list.ru>
2021-04-16 17:04:36 +0300

    Checked the colors in the figma project and created new color names.

--- src/colors.js
+++ src/colors.js
@@ -16,3 +16,45 @@ export const colors = {
   white60: 'rgba(255, 255, 255, 0.6)',
   iconColor: '#dadada',
 };
+
+const newColors = {
+  // color:
+  primaryColor: '#BB86FC', // programme name, add note round button background, open button, cloud button, progress bar, active(??) notes
+  secondaryColor: '#70EFDE', // notes, branches notes, frame notes
+  iconColor: '#000000', // icon in round button
+  textColor: 'rgba(255, 255, 255, 0.6)', // icon, placeholder, text popup, upload percents
+  titleColor: 'rgba(255, 255, 255, 0.87)', // menu, menu item, title notes, title popup, upload filenames, upload round button background
+  // background
+  systemBarBack: '#121212', // system bar background
+  mainBackground: 'rgba(255, 255, 255, 0.07), #121212', // main background
+  menuBackground: 'rgba(255, 255, 255, 0.12), #121212', // menu item background
+  menuSelectBackground: 'rgba(255, 255, 255, 0.12)', // selected menu item background
+  fileBackground: 'rgba(196, 196, 196, 0.5)', // file manager background
+  noteBackground: 'rgba(187, 134, 252, 0.08)', // create/edit note background
+  barBackground: 'rgba(255, 255, 255, 0.09), #121212', // top bar background, bottom bar background
+  frameHover: '#3C78C8', // :hover frame notes
+  // delete ??
+  States_white_overlay_Focus: 'rgba(255, 255, 255, 0.12)', // duplicate
+  Darkener_Background: 'rgba(0, 0, 0, 0.6)', // not used??
+  Primary_100: '#DBB2FF', // not used??
+  Elevation_Overlay_01dp: 'rgba(255, 255, 255, 0.05), #121212', // not used??
+};
+
+const figmaColors = {
+  Surface: '#121212', // system bar background
+  Surface_Overlay: 'rgba(255, 255, 255, 0.12)', // selected menu item background
+  States_white_overlay_Focus: 'rgba(255, 255, 255, 0.12)', // duplicate
+  Darkener_Background: 'rgba(0, 0, 0, 0.6)', // not used??
+  Primary_100: '#DBB2FF', // not used??
+  Primary_200: '#BB86FC', // programme name, add note round button, open button, cloud button, progress bar, active(??) notes
+  Secondary_100: '#70EFDE', // notes, branches notes, frame notes
+  On_Primary_High_Emphasis: '#000000', // icon in round button
+  On_Surface_Medium_Emphasis: 'rgba(255, 255, 255, 0.6)', // icon, placeholder, text popup, upload percents
+  On_Surface_High_Emphasis: 'rgba(255, 255, 255, 0.87)', // menu, menu item, title notes, title popup, upload filenames, upload round button
+  States_primary_overlay_Selected: 'rgba(187, 134, 252, 0.08)', // create/edit note background
+  Elevation_Overlay_01dp: 'rgba(255, 255, 255, 0.05), #121212', // not used??
+  Elevation_Overlay_02dp: 'rgba(255, 255, 255, 0.07), #121212', // main background
+  Elevation_Overlay_03dp: 'rgba(255, 255, 255, 0.09), #121212', // top bar background, bottom bar background
+  Elevation_Overlay_08dp: 'rgba(255, 255, 255, 0.12), #121212', // menu item background
+  frame_hover: '#3C78C8', // :hover frame notes
+};

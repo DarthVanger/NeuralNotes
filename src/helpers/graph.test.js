f5532ef7136525d187662fb58ad9294993480ed3
DarthVanger <darthvanger@gmail.com>
2021-04-17 23:53:59 +0300

    Add test for getNodeChildren

--- src/helpers/graph.test.js
+++ src/helpers/graph.test.js
@@ -1,17 +1,39 @@
-import { nodeHasChildren } from './graph.js';
+import { nodeHasChildren, getNodeChildren } from './graph.js';
 
 const nodes = [{ id: '0' }, { id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }];
 
-describe('nodeHasChildren', () => {
-  it('should return true when node has a child', () => {
-    const node = nodes[0];
-    const edges = [{ from: node.id, to: '1' }];
-    expect(nodeHasChildren({ nodes, edges }, node)).toBe(true);
+describe('graph helper', () => {
+  describe('nodeHasChildren', () => {
+    it('should return true when node has a child', () => {
+      const node = nodes[0];
+      const edges = [{ from: node.id, to: '1' }];
+      expect(nodeHasChildren({ nodes, edges }, node)).toBe(true);
+    });
+
+    it('should return false when node has no children', () => {
+      const node = nodes[0];
+      const edges = [{ from: '1', to: '2' }];
+      expect(nodeHasChildren({ nodes, edges }, node)).toBe(false);
+    });
   });
 
-  it('should return false when node has no children', () => {
-    const node = nodes[0];
-    const edges = [];
-    expect(nodeHasChildren({ nodes, edges }, node)).toBe(false);
+  describe('getNodeChildren', () => {
+    it('should return node children when the node has children', () => {
+      const node = nodes[0];
+      const edges = [
+        { from: node.id, to: '1' },
+        { from: node.id, to: '2' },
+      ];
+      expect(getNodeChildren({ nodes, edges }, node)).toEqual([
+        nodes[1],
+        nodes[2],
+      ]);
+    });
+
+    it('should return empty array when the node has no children', () => {
+      const node = nodes[0];
+      const edges = [{ from: '1', to: '2' }];
+      expect(getNodeChildren({ nodes, edges }, node)).toEqual([]);
+    });
   });
 });

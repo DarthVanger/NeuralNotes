eca0beb7b0a117c6c4aea66e4d7c28d4e72e897f
Oleg Ganchev <og77@list.ru>
2021-04-22 14:48:16 +0300

    Move ThemeProvider from App to index, added AppWrapper with background.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store, history } from 'sagas';
 
+import { ThemeProvider } from '@material-ui/styles';
+import { theme } from 'theme';
+
 import { App } from 'components/App/App';
 
 import 'abortcontroller-polyfill/dist/polyfill-patch-fetch';
@@ -15,8 +18,10 @@ import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <GlobalStyle />
-      <App />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </ConnectedRouter>
   </Provider>,
   document.getElementById('app-root'),

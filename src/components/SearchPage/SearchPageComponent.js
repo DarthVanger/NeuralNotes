767caab259c774056a40728f6088ac8b3acf1ce5
Александр Толкачев <kadokan81@gmail.com>
2021-04-23 14:28:14 +0300

    removed unnecessary import from SearchPageComponent

--- src/components/SearchPage/SearchPageComponent.js
+++ src/components/SearchPage/SearchPageComponent.js
@@ -2,17 +2,14 @@ import React from 'react';
 
 import { Typography } from '@material-ui/core';
 import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import { makeStyles } from '@material-ui/core/styles';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import InsertDriveFileRoundedIcon from '@material-ui/icons/InsertDriveFileRounded';
 import { colors } from 'colors';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 import { searchResultClickedAction } from 'components/NotesMindMap/NotesMindMapActions';
 

aa6889d0315f9d38110c49cabf5cef623f07ced0
DarthVanger <darthvanger@gmail.com>
2021-07-23 15:48:31 +0300

    Fix bug with passing extra props to Node

--- src/components/NotesMindMap/MindMap/Node.js
+++ src/components/NotesMindMap/MindMap/Node.js
@@ -2,10 +2,10 @@ import React from 'react';
 import NodeBackground from './NodeBackground';
 
 export const Node = node => {
-  const { x, y, label, width, height, padding, ...attrs } = node;
+  const { x, y, label, width, height, padding } = node;
 
   return (
-    <g {...attrs} transform={`translate(${x} ${y})`}>
+    <g transform={`translate(${x} ${y})`}>
       <NodeBackground width={width} height={height} padding={padding} />
       <g transform={`translate(${padding}, -${padding})`}>
         <text transform={`translate(${-width / 2}, ${height / 2})`}>

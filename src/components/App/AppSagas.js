e0185c7ec2203dc7efb837a6980916ea4cf9c621
DarthVanger <darthvanger@gmail.com>
2021-08-24 23:41:07 +0300

    Handle refreshing page while not on mind map page
    Redirect to mind map page if the inital loaded page is:
    - change parent
    - note details
    These pages rely on data passed from the mind map via actions, and don't
    work properly if loaded by direct link.

--- src/components/App/AppSagas.js
+++ src/components/App/AppSagas.js
@@ -1,5 +1,4 @@
-import * as googleApi from 'api/googleApi';
-import auth from 'auth';
+import { push } from 'connected-react-router';
 import { toast } from 'react-toastify';
 import {
   put,
@@ -7,6 +6,8 @@ import {
   takeEvery,
 } from 'redux-saga/dist/redux-saga-effects-npm-proxy.cjs';
 
+import * as googleApi from 'api/googleApi';
+import auth from 'auth';
 import {
   LOAD_GOOGLE_API_ACTION,
   authorizedUserOpenedApp,
@@ -20,12 +21,31 @@ export function* loadGoogleApi() {
   yield put(loadGoogleApiSuccessAction());
 }
 
+function* handleLocationChange({ payload }) {
+  // if user refreshes page while changing parent note, or while creating or editing a note,
+  // redirect to the mind map, as those pages rely on data passed from mind map via actions.
+  if (payload.isFirstRendering) {
+    const {
+      location: { pathname },
+    } = payload;
+
+    if (pathname.includes('/notes/change-parent')) {
+      yield put(push('/notes'));
+    }
+
+    if (pathname.includes('/note/')) {
+      yield put(push('/notes'));
+    }
+  }
+}
+
 export function* appInit() {
   yield call([toast, toast.configure], {
     position: toast.POSITION.BOTTOM_RIGHT,
   });
 
   yield takeEvery(LOAD_GOOGLE_API_ACTION, loadGoogleApi);
+  yield takeEvery('@@router/LOCATION_CHANGE', handleLocationChange);
 
   if (!auth.signedIn()) {
     yield put(notAuthorizedUserOpenedApp());

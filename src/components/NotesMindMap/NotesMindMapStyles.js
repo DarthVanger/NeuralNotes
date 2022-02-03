ef81ed3f1241577cf2a82b6bc5b057cc77773665
DarthVanger <darthvanger@gmail.com>
2022-02-03 15:28:08 +0200

    Fix mind map svg height bug in Safari

--- src/components/NotesMindMap/NotesMindMapStyles.js
+++ src/components/NotesMindMap/NotesMindMapStyles.js
@@ -5,6 +5,9 @@ export const StyledNotesMindMap = styled.div`
   flex-grow: 1;
   background: ${colors.elevationOverlay02dp};
 
+  /* Without "display: flex", the "height: 100%" for the <svg> inside doesn't work in Safari */
+  display: flex;
+
   /* In order to implement keyboard shortcuts (to handle keydown events)
    * this div is made focusable (by setting tabindex="0" attribute).
    * By default when it's focused it gets a 1px blue outline. Remove this outline.
